a7a0e18454c60666639dec12eafd66675097a2ae
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-05-02 16:04:17 +0100

    changes to attributs of AppUsers model

--- Classes/Classes/Models/AppUsers.cs
+++ Classes/Classes/Models/AppUsers.cs
@@ -1,14 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace Classes.Models {
+   /// <summary>
+   /// attributs for all users
+   /// </summary>
    public class AppUsers {
 
       [Key] // PK
       public int Id { get; set; }
 
+      /// <summary>
+      /// user's name
+      /// </summary>
       public string Name { get; set; }
 
+      /// <summary>
+      /// user's birth date
+      /// </summary>
       public DateOnly BirthDate { get; set; }
+
+      /// <summary>
+      /// user's cell phone
+      /// </summary>
+      [StringLength(17)]
+      [RegularExpression("(+|00)?[0-9]{7,15}",
+                         ErrorMessage = "Please, write a valid cell phone number")]
+      public string CellPhone { get; set; }
 
    }
 }
